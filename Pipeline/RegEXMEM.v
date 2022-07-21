f206b5b77a48beda6ebf6b16ef3ef6fc48e2a1e7
Yufan Cao <caoyf20@mails.tsinghua.edu.cn>
2022-07-22 06:08:13 +0800

    exmem

--- Pipeline/RegEXMEM.v
+++ Pipeline/RegEXMEM.v
@@ -2,20 +2,48 @@ module RegEXMEM (clk,
                  reset,
                  IALUResult,
                  IMemWrData,
-                 IImmExt,
-                 IRs,
-                 IRt,
-                 IRd,
-                 IShamt,
+                 IWriteReg,
                  ICRegWrite,
                  ICMemtoReg,
-                 ICBranch,
                  ICMemRead,
                  ICMemWrite,
-                 ICRegDst,
-                 ICALUOp,
-                 ICALUSrc1,
-                 ICALUSrc2,
-                 );
+                 CFlush,
+                 OALUResult,
+                 OMemWrData,
+                 OWriteReg,
+                 OCRegWrite,
+                 OCMemtoReg,
+                 OCMemRead,
+                 OCMemWrite);
+    input clk, reset;
+    input [31:0] IALUResult, IMemWrData;
+    input [4:0] IWriteReg;
+    input [1:0] ICMemtoReg;
+    input ICRegWrite, ICMemRead, ICMemWrite;
+    input CFlush;
+    output reg [31:0] OALUResult, OMemWrData;
+    output reg [4:0] OWriteReg;
+    output reg [1:0] OCMemtoReg;
+    output reg OCRegWrite, OCMemRead, OCMemWrite;
+    
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            OCRegWrite <= 0;
+            OCMemRead  <= 0;
+            OCMemWrite <= 0;
+            end else if (CFlush) begin
+            OCRegWrite <= 0;
+            OCMemRead  <= 0;
+            OCMemWrite <= 0;
+            end else begin
+            OALUResult <= IALUResult;
+            OMemWrData <= IMemWrData;
+            OWriteReg  <= IWriteReg;
+            OCRegWrite <= ICRegWrite;
+            OCMemtoReg <= ICMemtoReg;
+            OCMemRead  <= ICMemRead;
+            OCMemWrite <= ICMemWrite;
+        end
+    end
     
 endmodule
