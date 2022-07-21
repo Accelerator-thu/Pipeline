71d59a7276eeb0f247c3b0c1a68c8961a7a77278
Yufan Cao <caoyf20@mails.tsinghua.edu.cn>
2022-07-22 05:01:42 +0800

    IDEX reset

--- Pipeline/RegIDEX.v
+++ Pipeline/RegIDEX.v
@@ -6,6 +6,7 @@ module RegIDEX (clk,
                 IRs,
                 IRt,
                 IRd,
+                IShamt,
                 ICRegWrite,
                 ICMemtoReg,
                 ICBranch,
@@ -22,6 +23,7 @@ module RegIDEX (clk,
                 ORs,
                 ORt,
                 ORd,
+                OShamt,
                 OCRegWrite,
                 OCMemtoReg,
                 OCBranch,
@@ -35,15 +37,22 @@ module RegIDEX (clk,
     // I: input; IC: input control; C: control; O: output; OC: output control;
     input clk, reset;
     input [31:0] IDataA, IDataB, IImmExt;
-    input [4:0] IRs, IRt, IRd;
+    input [4:0] IRs, IRt, IRd, IShamt;
     input ICRegWrite, ICMemtoReg, ICBranch, ICMemRead, ICMemWrite, ICRegDst, ICALUOp, ICALUSrc1, ICALUSrc2;
     input CFlush;
     output reg [31:0] ODataA, ODataB, OImmExt;
-    output reg [4:0] ORs, ORt, ORd;
+    output reg [4:0] ORs, ORt, ORd, OShamt;
     output reg OCRegWrite, OCMemtoReg, OCBranch, OCMemRead, OCMemWrite, OCRegDst, OCALUOp, OCALUSrc1, OCALUSrc2;
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            
+            ORs        <= 0;
+            ORt        <= 0;
+            ORd        <= 0;
+            OShamt     <= 0;
+            OCRegWrite <= 0;
+            OCBranch   <= 0;
+            OCMemRead  <= 0;
+            OCMemWrite <= 0;
         end
     end
     
