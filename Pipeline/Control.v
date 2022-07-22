1fdc85c5361113c0cbf9f884dafdf3125f260c96
Yufan Cao <caoyf20@mails.tsinghua.edu.cn>
2022-07-23 00:11:49 +0800

    ctrl

--- Pipeline/Control.v
+++ Pipeline/Control.v
@@ -1,5 +1,37 @@
 module Control (OpCode,
-                Funct);
+                Funct,
+                RegWrite,
+                MemtoReg,
+                MemWrite,
+                ALUControl,
+                ALUSrc1,
+                ALUSrc2,
+                RegDst,
+                Branch,
+                BranchType,
+                ExtOp,
+                LUOp,
+                PCSrc);
     input [5:0] OpCode, Funct;
+    output reg [1:0] MemtoReg;
+    output reg RegWrite, MemWrite, RegDst, Branch;
+    
+    // OpCode,
+    //                 Funct,
+    //                 RegWrite,
+    //                 MemtoReg,
+    //                 MemWrite,
+    //                 ALUControl,
+    //                 ALUSrc1,
+    //                 ALUSrc2,
+    //                 RegDst,
+    //                 Branch,
+    //                 BranchType,
+    //                 ExtOp,
+    //                 LUOp,
+    //                 PCSrc
+    
+    
+    
     
 endmodule
