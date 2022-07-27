d7f5b7044ba338c01d2d862a2705c285dc422af2
Yufan Cao <caoyf20@mails.tsinghua.edu.cn>
2022-07-27 22:01:46 +0800

    fwd

--- Pipeline/Forward.v
+++ Pipeline/Forward.v
@@ -1,76 +1,56 @@
-module Forward (FD_OpCode,
-                FD_Funct,
-                DE_Rt,
-                DE_Rs,
-                FD_Rs,
-                FD_Rt,
-                EM_Rt,
-                EM_RegWrite,
-                MW_RegWrite,
-                EM_RegWrAddr,
-                MW_RegWrAddr,
-                ForwardA,
-                ForwardB,
-                ForwardC,
-                ForwardD,
-                ForwardE,
-                ForwardF);
-    input [5:0] FD_Funct;
-    input [5:0] FD_OpCode;
-    input [4:0] DE_Rt;
-    input [4:0] DE_Rs;
-    input [4:0] FD_Rs;
-    input [4:0] FD_Rt;
-    input [4:0] EM_Rt;
-    input EM_RegWrite;
-    input MW_RegWrite;
-    input [4:0] EM_RegWrAddr;
-    input [4:0] MW_RegWrAddr;
-    output reg [1:0] ForwardA;
-    output reg [1:0] ForwardB;
-    output reg [1:0] ForwardC;
-    output reg [1:0] ForwardD;
-    output reg ForwardE;
-    output reg ForwardF;
-    
-    always @(*) begin
-        if (EM_RegWrite && (EM_RegWrAddr ! = 0) && (EM_RegWrAddr == DE_Rs))
-            begin ForwardA <= 2'b01; end
-        else if (MW_RegWrite && (MW_RegWrAddr ! = 0) && (MW_RegWrAddr == DE_Rs) && ((EM_RegWrAddr ! = DE_Rs) || (~EM_RegWrite)))
-            begin ForwardA <= 2'b10; end
+module Forward (MEM_RegWrite,
+                MEM_WriteReg,
+                EX_Rs,
+                EX_Rt,
+                WB_RegWrite,
+                WB_WriteReg,
+                EX_ForwardA,
+                EX_ForwardB,
+                ID_Rs,
+                ID_Rt,
+                ID_Branch,
+                ID_PCSrc,
+                MEM_RegWrite,
+                MEM_WriteReg,
+                ID_ForwardA,
+                ID_ForwardB);
+    input MEM_RegWrite;
+    input [4:0] MEM_WriteReg;
+    input [31:0] EX_Rs, EX_Rt;
+    input WB_RegWrite;
+    input [4:0] WB_WriteReg;
+    output [1:0] EX_ForwardA, EX_ForwardB;
+    reg [1:0] EX_ForwardA, EX_ForwardB;
+    input [31:0] ID_Rs, ID_Rt;
+    input ID_Branch, MEM_RegWrite;
+    input [1:0] ID_PCSrc;
+    input [4:0] MEM_WriteReg;
+    output reg ID_ForwardA, ID_ForwardB;
+    always@(*)
+    begin
+        if ((ID_Branch || ID_PCSrc[1]) && MEM_WriteReg&& ID_Rs == MEM_WriteReg && MEM_RegWrite)
+            ID_ForwardA = 1;
         else
-            begin ForwardA <= 2'b00; end
+            ID_ForwardA = 0;
         
-        if (EM_RegWrite && (EM_RegWrAddr ! = 0) && (EM_RegWrAddr == DE_Rt))
-            begin ForwardB <= 2'b01; end
-        else if (MW_RegWrite && (MW_RegWrAddr ! = 0) && (MW_RegWrAddr == DE_Rt) && ((EM_RegWrAddr ! = DE_Rt) || (~EM_RegWrite)))
-            begin ForwardB <= 2'b10; end
+        if ((ID_Branch || ID_PCSrc[1]) && MEM_WriteReg&& ID_Rt == MEM_WriteReg && MEM_RegWrite)
+            ID_ForwardB = 1;
         else
-            begin ForwardB <= 2'b00; end
+            ID_ForwardB = 0;
         
-        if (EM_RegWrite && (EM_RegWrAddr ! = 0) && EM_RegWrAddr == FD_Rs) //beq系列指令转发
-            begin ForwardC <= 2'b01; end
-        else if (MW_RegWrite && (MW_RegWrAddr ! = 0) && (MW_RegWrAddr == FD_Rs) && ((EM_RegWrAddr ! = FD_Rs) || (~EM_RegWrite)))
-            begin ForwardC <= 2'b10; end
+        if (MEM_RegWrite && MEM_WriteReg&& MEM_WriteReg == EX_Rs)
+            EX_ForwardA = 2'b10;
+        else if (WB_RegWrite && WB_WriteReg&& WB_WriteReg == EX_Rs && (MEM_WriteReg ! = EX_Rs || ~MEM_RegWrite))
+            EX_ForwardA = 2'b01;
         else
-            begin ForwardC <= 2'b00; end
+            EX_ForwardA = 2'b00;
         
-        if (EM_RegWrite && (EM_RegWrAddr ! = 0) && EM_RegWrAddr == FD_Rt) //beq系列指令转发
-            begin ForwardD <= 2'b01; end
-        else if (MW_RegWrite && (MW_RegWrAddr ! = 0) && (MW_RegWrAddr == FD_Rt) && ((EM_RegWrAddr ! = FD_Rt) || (~EM_RegWrite)))
-            begin ForwardD <= 2'b10; end
+        if (MEM_RegWrite && MEM_WriteReg&& MEM_WriteReg == EX_Rt)
+            EX_ForwardB = 2'b10;
+        else if (WB_RegWrite && WB_WriteReg&& WB_WriteReg == EX_Rt && (MEM_WriteReg ! = EX_Rt || ~MEM_RegWrite))
+            EX_ForwardB = 2'b01;
         else
-            begin ForwardD <= 2'b00; end
-        
-        if (EM_RegWrAddr == FD_Rs && (FD_OpCode == 6'h00 && (FD_Funct == 6'h08 || FD_Funct == 6'h09))) //jal接jr
-            begin ForwardE <= 1'b1; end
-        else
-            begin ForwardE <= 1'b0; end
-        
-        if (MW_RegWrite && (MW_RegWrAddr ! = 0) && (MW_RegWrAddr == EM_Rt)) //sw指令转发
-            begin ForwardF <= 1'b1; end
-        else
-            begin ForwardF <= 1'b0; end
+            EX_ForwardB = 2'b00;
         
     end
 endmodule
