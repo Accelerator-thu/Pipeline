c44b36d1db83b862d345ada2309b6f72308d1aa1
Yufan Cao <caoyf20@mails.tsinghua.edu.cn>
2022-07-28 04:12:36 +0800

    typo

--- Pipeline/Pipeline_test.v
+++ Pipeline/Pipeline_test.v
@@ -2,7 +2,7 @@ module Pipeline_test ();
     
     reg clk, reset;
     wire [3:0] AN;
-    wire [7:0] BCD,
+    wire [7:0] BCD;
     wire [15:0] led;
     
     Pipeline CPU(clk, reset, AN, BCD, led);
